a26cc006a4a797b91cca6007d99a0c9956384995
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2024-08-20 10:59:40 +0100

    Converted prints to python logging

--- aws_lambda/main.py
+++ aws_lambda/main.py
@@ -5,6 +5,7 @@ import policy_checks
 import json
 import boto3
 import os
+import logging
 
 org = os.getenv("GITHUB_ORG")
 client_id = os.getenv("GITHUB_APP_CLIENT_ID")
@@ -16,70 +17,95 @@ secret_reigon = os.getenv("AWS_DEFAULT_REGION")
 account = os.getenv("AWS_ACCOUNT_NAME")
 bucket_name = f"{account}-github-audit-dashboard"
 
+logger = logging.getLogger()
+
 def handler(event, context):
 
-    print("Starting Execution")
+    logger.info("Starting Process")
+
+    logger.info("Creating Boto3 Session")
 
     # Create a boto3 session
     session = boto3.Session()
 
-    print("Created Boto3 Session")
+    logger.info("Created Boto3 Session")
+
+    logger.info("Creating Secrets Manager Client")
 
     # Get the .pem file from AWS Secrets Manager
     secret_manager = session.client("secretsmanager", region_name=secret_reigon)
 
+    logger.info("Secrets Manager Client Created")
+
+    logger.info("Getting secret from Secrets Manager")
+
     secret = secret_manager.get_secret_value(SecretId=secret_name)["SecretString"]
 
-    print("Got .pem file from AWS Secrets Manager")
+    logger.info("Got Secret from Secrets Manager")
+
+    logger.info("Getting access token")
 
     token = github_api_toolkit.get_token_as_installation(org, secret, client_id)
 
     # Check if the token is a string, if it is then an error occurred
     if type(token) == str:
-        print("Failed to get GitHub Access Token")
-        return
+        logger.error(f"Error getting token: {token}")
+        return f"Error getting token: {token}"
 
-    print("Got GitHub Access Token")
+    else:
+        logger.info("Got GitHub Access Token")
+
+    logger.info("Creating API Controller")
 
     gh = github_api_toolkit.github_interface(token[0])
 
-    print("Created GitHub API Controller")
+    logger.info("Created API Controller")
+
+    logger.info("Getting Repository Data")
 
     repos = policy_checks.get_repository_data(gh, org)
 
-    print("Got Repository Data")
+    logger.info("Repository Data Retrieved")
 
     with open("/tmp/repositories.json", "w") as f:
         f.write(json.dumps(repos, indent=4))
+
+    logger.info("Getting Secret Scanning Alerts")
 
     secret_scanning_alerts = policy_checks.get_security_alerts(gh, org, 5, "secret_scanning")
 
     with open("/tmp/secret_scanning.json", "w") as f:
         f.write(json.dumps(secret_scanning_alerts, indent=4))
 
-    print("Got Secret Scanning Alerts")
+    logger.info("Secret Scanning Alerts Retrieved")
+
+    logger.info("Getting Dependabot Alerts")
 
     dependabot_alerts = policy_checks.get_all_dependabot_alerts(gh, org)
 
     with open("/tmp/dependabot.json", "w") as f:
         f.write(json.dumps(dependabot_alerts, indent=4))
 
-    print("Got Dependabot Alerts")
+    logger.info("Dependabot Alerts Retrieved")
+
+    logger.info("Pushing Data to S3")
+
+    logger.info("Creating S3 Client")
 
     s3 = session.client('s3')
 
-    print("Created S3 Client")
+    logger.info("Created S3 Client")
 
     s3.upload_file("/tmp/repositories.json", bucket_name, "repositories.json")
 
-    print("Uploaded Repositories JSON to S3")
+    logger.info("Uploaded Repositories JSON to S3")
 
     s3.upload_file("/tmp/secret_scanning.json", bucket_name, "secret_scanning.json")
 
-    print("Uploaded Secret Scanning JSON to S3")
+    logger.info("Uploaded Secret Scanning JSON to S3")
 
     s3.upload_file("/tmp/dependabot.json", bucket_name, "dependabot.json")
 
-    print("Uploaded Dependabot JSON to S3")
+    logger.info("Uploaded Dependabot JSON to S3")
 
-    print("Finished Execution")
+    logger.info("Process Complete")
