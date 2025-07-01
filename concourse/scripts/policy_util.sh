4086d488335bd43406b01446e171390d9cef7df9
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-01 09:15:58 +0100

    export the repo name on the terraform step

--- concourse/scripts/policy_util.sh
+++ concourse/scripts/policy_util.sh
@@ -1,4 +1,4 @@
-if [[ "${repo_name}" == "github-policy-dashboard" || ${repo_name} == "github-policy-lambda" ]]; then
+if [[ "${repo_name}" == "github-policy-dashboard" || "${repo_name}" == "github-policy-lambda" ]]; then
     echo "${repo_name}"
 else
     echo "Unknown repository name: ${repo_name}"
