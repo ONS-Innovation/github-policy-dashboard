bf93295f09e3149f1e445e87d9eaee678edf35e5
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-01 08:47:29 +0100

    add space before ]]

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -5,7 +5,7 @@ export PODMAN_SYSTEMD_UNIT=concourse-task
 
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
-if [[ ${repo_name} == "github-policy-dashboard"]]; then
+if [[ ${repo_name} == "github-policy-dashboard" ]]; then
     echi "TEST 1"
     container_image=$(echo "$secrets" | jq -r .container_image)
     podman build -t ${container_image}:${tag} resource-repo
