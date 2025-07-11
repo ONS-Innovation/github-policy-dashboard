b95bcaf3786f1142498febaa92fd9fc52e027a53
delterr <Hasan.T.Ahmad@ons.gov.uk>
2025-07-11 10:02:02 +0100

    update README and remove redundant code

--- concourse/scripts/build_image.sh
+++ concourse/scripts/build_image.sh
@@ -6,11 +6,9 @@ export PODMAN_SYSTEMD_UNIT=concourse-task
 aws ecr get-login-password --region eu-west-2 | podman --storage-driver=vfs login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.eu-west-2.amazonaws.com
 
 if [[ ${repo_name} == "github-policy-dashboard" ]]; then
-    echo "TEST 1"
     container_image=$(echo "$secrets" | jq -r .container_image)
     podman build -t ${container_image}:${tag} resource-repo
 else 
-    echo "TEST 2"
     container_image=$(echo "$secrets" | jq -r .container_image_lambda)
     podman build -t ${container_image}:${tag} resource-repo/data_logger/
 fi
